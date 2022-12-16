4d1fa7cb30af9c7bc8f47d3b71eda334126c751d
tranxuannghiep <nghiepradeon@gmailo.com>
2022-12-16 13:16:02 +0700

    getStaticProps and getStaticPaths

--- pages/post/[postId].tsx
+++ pages/post/[postId].tsx
@@ -1,15 +1,52 @@
+import { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from 'next';
 import { useRouter } from 'next/router';
 import * as React from 'react';
 
-export interface PostDetailPageProps {}
+export interface PostDetailPageProps {
+  post: any;
+}
 
-export default function PostDetailPage(props: PostDetailPageProps) {
+export default function PostDetailPage({ post }: PostDetailPageProps) {
   const router = useRouter();
 
   return (
     <div>
       Post Detail page
-      <p>Query {JSON.stringify(router.query)}</p>
+      <p>{post.id}</p>
+      <p>{post.title}</p>
+      <p>{post.body}</p>
     </div>
   );
 }
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [
+      { params: { postId: '1' } },
+      { params: { postId: '2' } },
+      { params: { postId: '3' } },
+      { params: { postId: '4' } },
+      { params: { postId: '5' } },
+      { params: { postId: '6' } },
+      { params: { postId: '7' } },
+      { params: { postId: '8' } },
+      { params: { postId: '9' } },
+      { params: { postId: '10' } },
+    ],
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps<PostDetailPageProps> = async (
+  context: GetStaticPropsContext
+) => {
+  const postId = context.params?.postId;
+  if (!postId) return { notFound: true };
+  const fetchData = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`);
+  const data = await fetchData.json();
+  return {
+    props: {
+      post: data,
+    },
+  };
+};
--- pages/post/index.tsx
+++ pages/post/index.tsx
@@ -1,7 +1,35 @@
+import { GetStaticProps, GetStaticPropsContext } from 'next';
+import { useRouter } from 'next/router';
 import * as React from 'react';
 
-export interface PostListPageProps {}
-
-export default function PostListPage(props: PostListPageProps) {
-  return <div>Post List Page</div>;
+export interface PostListPageProps {
+  posts: any[];
 }
+
+export default function PostListPage({ posts }: PostListPageProps) {
+  const router = useRouter();
+  return (
+    <div>
+      <h1>Post List Page</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id} onClick={() => router.push(`post/${post.id}`)}>
+            {post.title}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export const getStaticProps: GetStaticProps<PostListPageProps> = async (
+  context: GetStaticPropsContext
+) => {
+  const fetchData = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=10');
+  const data = await fetchData.json();
+  return {
+    props: {
+      posts: data.map((post: any) => ({ id: post.id, title: post.title })),
+    },
+  };
+};
