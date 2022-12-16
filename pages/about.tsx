2d2be4838a64729713ede22dad8d28db1c4c42c8
tranxuannghiep <nghiepradeon@gmailo.com>
2022-12-16 14:30:32 +0700

    SSG and data fetching on client side

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,7 +1,51 @@
-import * as React from 'react';
+import { GetStaticProps, GetStaticPropsContext } from 'next';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 
 export interface AboutPageProps {}
 
 export default function AboutPage(props: AboutPageProps) {
-  return <div>About Page</div>;
+  const [postList, setPostList] = useState<any[]>([]);
+
+  const router = useRouter();
+  const page = router.query?.page;
+
+  useEffect(() => {
+    if (!page) return;
+    (async () => {
+      const fetchData = await fetch(`https://js-post-api.herokuapp.com/api/posts?_page=${page}`);
+      const data = await fetchData.json();
+      setPostList(data.data);
+    })();
+  }, [page]);
+
+  const handleClickNext = () => {
+    router.push(
+      {
+        pathname: '/about',
+        query: {
+          page: Number(page || 1) + 1,
+        },
+      },
+      undefined,
+      { shallow: true }
+    );
+  };
+  return (
+    <div>
+      <h1>About Page</h1>
+      <ul>
+        {postList.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+      <button onClick={handleClickNext}>Next Page</button>
+    </div>
+  );
 }
+
+export const getStaticProps: GetStaticProps = async (context: GetStaticPropsContext) => {
+  return {
+    props: {},
+  };
+};
