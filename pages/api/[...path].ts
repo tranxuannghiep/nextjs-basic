efd84e1f98fb050aa038aaa91bdcf1291999b4e9
NghiepRadeon <nghiepradeon@gmail.com>
2022-12-18 00:42:06 +0700

    setup cookies

--- pages/api/[...path].ts
+++ pages/api/[...path].ts
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
 import httpProxy from 'http-proxy';
+import Cookies from 'cookies';
 
 const proxy = httpProxy.createProxyServer();
 
@@ -11,12 +12,23 @@ export const config = {
 };
 
 export default function handler(req: NextApiRequest, res: NextApiResponse<any>) {
-  req.headers.cookie = '';
+  return new Promise((resolve) => {
+    const cookies = new Cookies(req, res);
+    const access_token = cookies.get('access_token');
+    if (access_token) {
+      req.headers.authorization = 'Bearer ' + access_token;
+    }
 
-  proxy.web(req, res, {
-    target: process.env.API_URL,
-    changeOrigin: true,
-    selfHandleResponse: false,
+    req.headers.cookie = '';
+
+    proxy.web(req, res, {
+      target: process.env.API_URL,
+      changeOrigin: true,
+      selfHandleResponse: false,
+    });
+
+    proxy.once('proxyRes', () => {
+      resolve(true);
+    });
   });
-  // res.status(200).json({ name: 'John Doe' });
 }
---
+++ pages/api/login.ts
@@ -0,0 +1,57 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+import httpProxy, { ProxyResCallback } from 'http-proxy';
+import Cookies from 'cookies';
+
+const proxy = httpProxy.createProxyServer();
+
+interface Data {
+  message: string;
+}
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  if (req.method !== 'POST') {
+    res.status(404).json({ message: 'method not supported' });
+  }
+  return new Promise((resolve) => {
+    req.headers.cookie = '';
+
+    const handleLoginResponse: ProxyResCallback = (proxyRes, req, res) => {
+      let body = '';
+
+      proxyRes.on('data', function (chunk) {
+        body += chunk;
+      });
+
+      proxyRes.on('end', function () {
+        try {
+          const { accessToken, expiredAt } = JSON.parse(body);
+          const cookies = new Cookies(req, res, { secure: process.env.NODE_ENV !== 'development' });
+          cookies.set('access_token', accessToken, {
+            httpOnly: true,
+            sameSite: 'lax',
+            expires: new Date(expiredAt),
+          });
+          (res as NextApiResponse).status(200).json({ message: 'Login successfully' });
+        } catch (error) {
+          (res as NextApiResponse).status(500).json({ message: 'something went wrong' });
+        }
+        resolve(true);
+      });
+    };
+
+    proxy.once('proxyRes', handleLoginResponse);
+
+    proxy.web(req, res, {
+      target: process.env.API_URL,
+      changeOrigin: true,
+      selfHandleResponse: true,
+    });
+  });
+}
