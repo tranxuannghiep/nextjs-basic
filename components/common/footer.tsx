f5e345b2ebb3875e4361d27fd13e474853084805
NghiepRadeon <nghiepradeon@gmail.com>
2022-12-20 23:39:56 +0700

    footer

--- components/common/footer.tsx
+++ components/common/footer.tsx
@@ -1,9 +1,42 @@
-import { Box } from '@mui/material';
+import { Facebook, Instagram, Twitter, LinkedIn } from '@mui/icons-material';
+import { Box, Icon, Stack, Typography } from '@mui/material';
 
 export function Footer() {
+  const socialLinks = [
+    {
+      icon: Facebook,
+      url: 'https://www.google.com/',
+    },
+    {
+      icon: Instagram,
+      url: 'https://www.google.com/',
+    },
+    {
+      icon: Twitter,
+      url: 'https://www.google.com/',
+    },
+    {
+      icon: LinkedIn,
+      url: 'https://www.google.com/',
+    },
+  ];
   return (
-    <Box component="footer" textAlign="center">
-      Footer
+    <Box component="footer" textAlign="center" py={2}>
+      <Stack direction="row" justifyContent="center">
+        {socialLinks.map((social, index) => (
+          <Box
+            key={index}
+            component="a"
+            p={2}
+            href={social.url}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Icon component={social.icon} fontSize="large" />
+          </Box>
+        ))}
+      </Stack>
+      <Typography>Copyright ©{new Date().getFullYear()} All rights reserved </Typography>
     </Box>
   );
 }
