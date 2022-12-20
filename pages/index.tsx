80782cbac206fe2686b474f5f4de8d563116b2ad
NghiepRadeon <nghiepradeon@gmail.com>
2022-12-21 00:07:45 +0700

    seo home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { Seo } from '@/components/common/seo';
 import { FeatureWorks, HeroSection, RecentPosts } from '@/components/home';
 import { MainLayout } from '@/components/layout';
 import { Box } from '@mui/material';
@@ -6,6 +7,15 @@ import { NextPageWithLayout } from 'models';
 const Home: NextPageWithLayout = () => {
   return (
     <Box>
+      <Seo
+        data={{
+          title: 'NextJS Basic',
+          description: 'Home page NextJs basic',
+          url: 'https://nextjs-basic-gilt.vercel.app/',
+          thumbnailUrl:
+            'https://www.drupal.org/files/project-images/nextjs-icon-dark-background.png',
+        }}
+      />
       <HeroSection />
       <RecentPosts />
       <FeatureWorks />
