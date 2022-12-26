6e20aa5cd3c76b27ca1d3dcd4cc6bc5bbe67c4e1
tranxuannghiep <nghiepradeon@gmailo.com>
2022-12-26 11:33:45 +0700

    Convert markdown to HTML using unified

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -3,6 +3,12 @@ import { Post } from '@/models';
 import { getPostList } from '@/utils/posts';
 import { Box, Typography } from '@mui/material';
 import { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from 'next';
+import { unified } from 'unified';
+import remarkParse from 'remark-parse';
+import remarkRehype from 'remark-rehype';
+import rehypeDocument from 'rehype-document';
+import rehypeFormat from 'rehype-format';
+import rehypeStringify from 'rehype-stringify';
 import * as React from 'react';
 
 export interface BlogDetailPageProps {
@@ -13,12 +19,12 @@ export default function BlogDetailPage({ post }: BlogDetailPageProps) {
   return (
     <Box mt={{ xs: 3, sm: 11 }}>
       <Typography component="h1" variant="h3" fontWeight="bold" mb={{ xs: 2, sm: 7 }}>
-        Post Detail
+        Blog Detail
       </Typography>
       <p>{post.id}</p>
       <p>{post.title}</p>
       <p>{post.author?.name}</p>
-      <p>{post.mdContent}</p>
+      <div dangerouslySetInnerHTML={{ __html: post.htmlContent || '' }}></div>
     </Box>
   );
 }
@@ -41,6 +47,15 @@ export const getStaticProps: GetStaticProps<BlogDetailPageProps> = async (
   if (!slug) return { notFound: true };
   const post = postList.find((post) => post.slug === slug);
   if (!post) return { notFound: true };
+  const file = await unified()
+    .use(remarkParse)
+    .use(remarkRehype)
+    .use(rehypeDocument, { title: 'Blog detail' })
+    .use(rehypeFormat)
+    .use(rehypeStringify)
+    .process(post.mdContent || '');
+  post.htmlContent = file.toString();
+
   return {
     props: {
       post,
