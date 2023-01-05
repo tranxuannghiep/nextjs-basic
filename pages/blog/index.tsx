aef8835efbfdfab4b436ba55706e242c22e199dd
NghiepRadeon <nghiepradeon@gmail.com>
2023-01-05 22:27:41 +0700

    wrapped blog page

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,5 +1,6 @@
 import { PostItem } from '@/components/common/blog';
 import { MainLayout } from '@/components/layout';
+import { ProtectedRoute } from '@/components/protected/ProtectedRoute ';
 import { Post } from '@/models';
 import { getPostList } from '@/utils/posts';
 import { Box, Divider, Typography } from '@mui/material';
@@ -11,19 +12,21 @@ export interface BlogListPageProps {
 
 export default function BlogListPage({ posts }: BlogListPageProps) {
   return (
-    <Box mt={{ xs: 3, sm: 11 }}>
-      <Typography component="h1" variant="h3" fontWeight="bold" mb={{ xs: 2, sm: 7 }}>
-        Blog
-      </Typography>
-      {posts.map((post) => (
-        <Link key={post.id} href={`blog/${post.slug}`} passHref>
-          <Box>
-            <PostItem post={post} />
-            <Divider sx={{ my: { xs: 2, sm: 4 } }} />
-          </Box>
-        </Link>
-      ))}
-    </Box>
+    <ProtectedRoute>
+      <Box mt={{ xs: 3, sm: 11 }}>
+        <Typography component="h1" variant="h3" fontWeight="bold" mb={{ xs: 2, sm: 7 }}>
+          Blog
+        </Typography>
+        {posts.map((post) => (
+          <Link key={post.id} href={`blog/${post.slug}`} passHref>
+            <Box>
+              <PostItem post={post} />
+              <Divider sx={{ my: { xs: 2, sm: 4 } }} />
+            </Box>
+          </Link>
+        ))}
+      </Box>
+    </ProtectedRoute>
   );
 }
 
