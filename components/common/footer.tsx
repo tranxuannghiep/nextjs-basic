e206cc0c3c3a91ddd09b1fad07a3f158a3b2fee4
tranxuannghiep <nghiepradeon@gmailo.com>
2023-01-10 17:50:34 +0700

    test

--- components/common/footer.tsx
+++ components/common/footer.tsx
@@ -1,28 +1,28 @@
 import { Facebook, Instagram, Twitter, LinkedIn } from '@mui/icons-material';
-import { Box, Typography } from '@mui/material';
+import { Box, Stack, Typography } from '@mui/material';
 
 export function Footer() {
-  // const socialLinks = [
-  //   {
-  //     icon: Facebook,
-  //     url: 'https://www.google.com/',
-  //   },
-  //   {
-  //     icon: Instagram,
-  //     url: 'https://www.google.com/',
-  //   },
-  //   {
-  //     icon: Twitter,
-  //     url: 'https://www.google.com/',
-  //   },
-  //   {
-  //     icon: LinkedIn,
-  //     url: 'https://www.google.com/',
-  //   },
-  // ];
+  const socialLinks = [
+    {
+      icon: Facebook,
+      url: 'https://www.google.com/',
+    },
+    {
+      icon: Instagram,
+      url: 'https://www.google.com/',
+    },
+    {
+      icon: Twitter,
+      url: 'https://www.google.com/',
+    },
+    {
+      icon: LinkedIn,
+      url: 'https://www.google.com/',
+    },
+  ];
   return (
     <Box component="footer" textAlign="center" py={2}>
-      {/* <Stack direction="row" justifyContent="center">
+      <Stack direction="row" justifyContent="center">
         {socialLinks.map((social, index) => (
           <Box
             key={index}
@@ -32,10 +32,10 @@ export function Footer() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <Icon component={social.icon} fontSize="large" />
+            {/* <Icon component={social.icon} fontSize="large" /> */}
           </Box>
         ))}
-      </Stack> */}
+      </Stack>
       <Typography>Copyright ©{new Date().getFullYear()} All rights reserved </Typography>
     </Box>
   );
